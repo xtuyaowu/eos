4c0bb923e53a4c7b0d701857d3f0c89a3f0d9d32
yaowu <toyaowu@163.com>
2018-07-30 16:28:22 +0800

    uncomment

--- contracts/eosio.token/eosio.token.cpp
+++ contracts/eosio.token/eosio.token.cpp
@@ -80,20 +80,20 @@ void token::transfer( account_name from,
     eosio_assert( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
     eosio_assert( memo.size() <= 256, "memo has more than 256 bytes" );
 
-    auto fee = quantity;                         // 新增
-    eosio::print("################# fee.amount 1: ", fee.amount, "\n");
-    fee.amount = ( fee.amount * 0.0002 );         // 新增
-    eosio::print("################# fee.amount 2: ", fee.amount, "\n");
-    auto quantity_after_fee = quantity;          // 新增
-    eosio::print("################# quantity_after_fee.amount 1: ", quantity_after_fee.amount, "\n");
-    quantity_after_fee.amount -= fee.amount;     // 新增
-    eosio::print("################# quantity_after_fee.amount 2: ", quantity_after_fee.amount, "\n");
+    // auto fee = quantity;                         // 新增
+    // eosio::print("################# fee.amount 1: ", fee.amount, "\n");
+    // fee.amount = ( fee.amount * 0.0002 );         // 新增
+    // eosio::print("################# fee.amount 2: ", fee.amount, "\n");
+    // auto quantity_after_fee = quantity;          // 新增
+    // eosio::print("################# quantity_after_fee.amount 1: ", quantity_after_fee.amount, "\n");
+    // quantity_after_fee.amount -= fee.amount;     // 新增
+    // eosio::print("################# quantity_after_fee.amount 2: ", quantity_after_fee.amount, "\n");
 
     sub_balance( from, quantity );
     add_balance( to, quantity, from );
 
-    INLINE_ACTION_SENDER(eosio::token, transfer)( N(eosio.token), {from,N(active)},
-    { from, N(eosio.txsfee), fee, std::string("pay txsfee") } ); // 新增
+    // INLINE_ACTION_SENDER(eosio::token, transfer)( N(eosio.token), {from,N(active)},
+    // { from, N(eosio.txsfee), fee, std::string("pay txsfee") } ); // 新增
 }
 
 void token::sub_balance( account_name owner, asset value ) {
